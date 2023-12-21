0080114f99e2109ffa10d0524750414e769d1a7c
Sebastian Uhlig <sebastian-uhlig@outlook.com>
2023-12-21 21:40:25 +0100

    chore: make 404 error page customizable

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,20 +1,28 @@
 @php($page = \App\Models\Page::where('slug', '=', '404')->first())
 
-<x-layouts.app :page="$page">
-    <div class="max-w-7xl mx-auto p-6 lg:p-8 w-full">
-        <div class="mb-8">
-            <x:html-tags.h1 class="mb-6">{{ $page->h1 }}</x:html-tags.h1>
-            @if(!empty($page->subtitle))
-                <x:html-tags.h2 class="mb-6">{{ $page->subtitle }}</x:html-tags.h2>
+@if (!blank($page))
+    <x-layouts.app :page="$page">
+        <div class="max-w-7xl mx-auto p-6 lg:p-8 w-full">
+            <div class="mb-8">
+                <x:html-tags.h1 class="mb-6">{{ $page->h1 }}</x:html-tags.h1>
+                @if(!empty($page->subtitle))
+                    <x:html-tags.h2 class="mb-6">{{ $page->subtitle }}</x:html-tags.h2>
+                @endif
+            </div>
+
+            @if(!empty($page->content))
+                <div class="flex flex-col space-y-8 lg:space-y-16">
+                    @foreach($page->content as $content)
+                        {!! $page->prepareContent($content) !!}
+                    @endforeach
+                </div>
             @endif
         </div>
+    </x-layouts.app>
+@else
+    @extends('errors::minimal')
 
-        @if(!empty($page->content))
-            <div class="flex flex-col space-y-8 lg:space-y-16">
-                @foreach($page->content as $content)
-                    {!! $page->prepareContent($content) !!}
-                @endforeach
-            </div>
-        @endif
-    </div>
-</x-layouts.app>
+    @section('title', __('Not Found'))
+    @section('code', '404')
+    @section('message', __('Not Found'))
+@endif
